34a3f3080cf708d4833656b85f5e1f0ba1479987
Gregor <gregor.mitscha-baude@gmx.at>
2022-06-23 12:39:21 +0200

    add timings to roll-up console logs

--- src/run-recursive.ts
+++ src/run-recursive.ts
@@ -18,60 +18,69 @@ import {
   MastermindHelper,
   MastermindState,
 } from './Mastermind-recursive.js';
+import { tic, toc } from './tictoc.js';
 
 await isReady;
 
-console.log('compiling...');
+tic('compiling');
 await Mastermind.compile();
+toc();
 
 let solution = new Pegs([1, 2, 3, 6]);
 
 // initialize (== create the first proof)
-console.log('prove... (init)');
+tic('prove (init)');
 let initialState = MastermindHelper.init(solution);
 let initialProof = await Mastermind.init(initialState, solution); // <-- no class instantiation, just calling a function to create proof
+toc();
 
 console.log('Proof state initialized!');
 
 // to make a guess, a user would fetch the initial proof from a server, and then run this:
 
-console.log('prove... (guess)');
+tic('prove (guess)');
 let guess = new Pegs([6, 2, 1, 3]);
 let userState = MastermindHelper.publishGuess(guess, initialProof);
 let userProof = await Mastermind.publishGess(userState, guess, initialProof);
+toc();
 
 console.log('Guess Valid!');
 
 // user would now post the userProof to the server, and wait for it to publish a hint in form of another proof
 
-console.log('prove... (hint)');
+tic('prove (hint)');
 let serverState = MastermindHelper.publishHint(solution, userProof);
 let serverProof = await Mastermind.publishHint(
   serverState,
   solution,
   userProof
 );
+toc();
 
 console.log('Red: ' + serverProof.publicInput.redPegs.toString());
 console.log('White: ' + serverProof.publicInput.whitePegs.toString());
 
 // back to the user, who makes another guess:
 
-console.log('prove... (guess)');
+tic('prove (guess)');
 guess = new Pegs([1, 2, 3, 6]);
 userState = MastermindHelper.publishGuess(guess, serverProof);
 userProof = await Mastermind.publishGess(userState, guess, serverProof);
+toc();
 
 console.log('Guess Valid!');
 
 // server published another hint:
 
-console.log('prove... (hint)');
+tic('prove (hint)');
 serverState = MastermindHelper.publishHint(solution, userProof);
 let finalProof = await Mastermind.publishHint(serverState, solution, userProof);
+toc();
 
 console.log('Red: ' + finalProof.publicInput.redPegs.toString());
 console.log('White: ' + finalProof.publicInput.whitePegs.toString());
+
+console.log('Got final proof!');
 
 // the serverProof that we have now has a publicInput with redPegs === 4, which means the game is won
 // if you verify it, you *know* that someone ran the methods above to produce this winning state
@@ -113,20 +122,26 @@ Mina.setActiveInstance(Local);
 const publisherAccount = Local.testAccounts[0].privateKey;
 
 // deploy
+tic('compile & deploy rollup');
 await MastermindRollup.compile(zkAppAddress);
 let tx = await Mina.transaction(publisherAccount, () => {
   Party.fundNewAccount(publisherAccount);
   zkapp.deploy({ zkappKey: zkAppPrivateKey });
 });
 await tx.send().wait();
+toc();
 
 // prove that we have a proof that shows that we won
+tic('prove (rollup)');
 tx = await Mina.transaction(publisherAccount, () => {
   // call out method with final proof from the ZkProgram as argument
   zkapp.publishCompletedGame(finalProof);
 });
 await tx.prove();
 await tx.send().wait();
+toc();
+
+console.log('Did someone win?', zkapp.someoneWon.get().toBoolean());
 
 // this was only a single transaction, which proves the same thing as the many transactions in the non-recursive example!
 
